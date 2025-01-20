954be202620881ad3e9d0e31d47605d864d58d06
Krispyish <Jakoblesteryt@gmail.com>
2025-01-20 08:46:08 -0500

    Fixed bug that was happening when an 'Ace' entered a player's hand. It would reprompt every gameply loop for the value of the Ace

--- main.py
+++ main.py
@@ -20,42 +20,47 @@ class Deck:
     def get_score(cards):
         card_value = 0
         player_choice = ""
-        for card in cards:
-            if card[0] == "A":
-                while player_choice != 1 and player_choice != 11:
-                    player_choice = int(input("Would you like your 'Ace' to be 1 or 11?: "))
-                    if player_choice == 1:
-                        card_value += 1
-                    elif player_choice == 11:
-                        card_value += 11
-            elif card[0] == "J" or card[0] == "Q" or card[0] == "K":
+        last_card = cards[-1]
+        
+        if last_card[0] == "A":
+            while player_choice != 1 and player_choice != 11:
+                player_choice = int(input("Would you like your 'Ace' to be 1 or 11?: "))
+        if player_choice == 1:
+            card_value += 1
+        elif player_choice == 11:
+            card_value += 11
+        elif last_card[0] == "J" or last_card[0] == "Q" or last_card[0] == "K":
+            card_value += 10
+            
+        elif last_card[0] == "1":
+            if last_card[1] != 0:
+                card_value += 1
+            else:
                 card_value += 10
-            elif card[0] == "1":
-                if card[1] != 0:
-                    card_value += 1
-                else:
-                    card_value += 10
-            else:    
-                card_value += int(card[0])
+        else:    
+            card_value += int(last_card[0])
+        
         return card_value      
 
+
 def main():
+    dealer_hand = []
     player_hand = []
-    player_input = "" 
+    player_input = ""
+    player_hand_value = 0 
     while player_input != "fold":
         player_input = input("Draw or fold?: ")
         if player_input == "draw":
             Deck.deal_card(player_hand)
-            hand_value = Deck.get_score(player_hand)
-            print(f"Your hand: {player_hand} \nHand value: {hand_value}")
+            player_hand_value += Deck.get_score(player_hand)
+            print(f"Your hand: {player_hand} \nHand value: {player_hand_value}")
             
-            if hand_value > 21:
+            if player_hand_value > 21:
                 print("You bust")
                 break
-            elif hand_value == 21:
+            elif player_hand_value == 21:
                 print("Black Jack... You Win!")
-                break
-                      
+                break         
         elif player_input == "fold":
             print("You fold...")
               
